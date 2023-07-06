45795821eb58c73a237b7583348f2703f587f24f
Michail Chatzis <michatzis@gmail.com>
2023-07-06 10:08:41 +0300

    added user authentication

--- bars_app/urls.py
+++ bars_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from bars_app.views import HomeView
+from bars_app.views import HomeView, RegisterView
 
 
 app_name = "bars_app"
 
 urlpatterns = [
-    path("", HomeView.as_view(), name='home')
+    path("", HomeView.as_view(), name='home'),
+    path("register", RegisterView.as_view(), name='register'),
 ]
--- bars_app/views.py
+++ bars_app/views.py
@@ -1,5 +1,24 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
 
 class HomeView(TemplateView):
     template_name = "bars_app/index.html"
+
+class RegisterView(CreateView):
+    template_name = "registration/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def get_success_url(self):
+        next_page = self.request.GET.get('next', None)
+        if next_page is not None:
+            return reverse_lazy('login') + "?next=" + next_page
+        super().get_success_url()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['next'] = self.request.GET.get('next', None)
+        return context
