bc82e74810227a94125930da8691807cadb6bca0
Michail Chatzis <michatzis@gmail.com>
2023-08-24 14:51:27 +0300

    removed api-client from git

--- api-client/apis/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './BarTypesApi';
-export * from './BarsApi';
-export * from './SchemaApi';
--- api-client/models/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Bar';
-export * from './BarType';
-export * from './FeatureTypeEnum';
