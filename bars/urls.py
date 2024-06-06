297b0373941e86c547c1e867d9dfa02a8107ed0d
Michail Chatzis <michatzis@gmail.com>
2024-06-06 20:14:25 +0200

    removed admin routing

--- bars/urls.py
+++ bars/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    #path("admin/", admin.site.urls),
     path("", include('django.contrib.auth.urls')),
     path("", include('bars_app.urls')),
     path("api/", include('api.urls')),
